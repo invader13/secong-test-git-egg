bc83457b4f2b7c41319cfe54c805e81cab83baff
eduardo.gonzalez <invader@live.com.mx>
2025-02-11 13:40:50 -0600

    EGG- se implementa el botón de regresar

--- second-deploy-git/src/app/egg-card/egg-card.component.ts
+++ second-deploy-git/src/app/egg-card/egg-card.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
-import {MatCard, MatCardModule} from "@angular/material/card";
+import { MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import { Location } from '@angular/common';
+import { MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-egg-card',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './egg-card.component.html',
   styleUrl: './egg-card.component.scss'
 })
 export class EggCardComponent {
+  constructor(private _location: Location) {
+  }
 
+  backClicked() {
+    this._location.back();
+  }
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
